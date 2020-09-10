5463692c2767d325fd5b64da7cb882cb8185e5cd
César Aparicio <clark012389@hotmail.com>
2020-09-10 17:29:20 -0500

    Configuracion de proyecto base

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://github.com/icao/pruebas-unitarias-integracion-react'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Repositorio de pruebas unitarias e integracion en React.js
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
