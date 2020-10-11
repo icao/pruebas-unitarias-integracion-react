ecd03fafc1c063a117b2b8f5ae34fe04a63d3ee2
César Aparicio <clark012389@hotmail.com>
2020-10-11 01:15:58 -0500

    Probando que la funcion getHeroById retorna un heroe existente en la data

--- src/_tests_/base/04-imp-exp-test.test.js
+++ src/_tests_/base/04-imp-exp-test.test.js
@@ -1,9 +1,14 @@
 import { getHeroById, getHeroByOwner } from '../../base/04-imp-exp'
+import heroes from '../../data/heroes'
+
 describe('Pruebas en 04-imp-exp', () => {
-  test('Mostrar arreglo', () => {
-    const heroe = getHeroById(1)
-    const heroes = getHeroByOwner('Marvel')
-    console.log('REGRESA HEROE CON ID = 1: ', heroe)
-    console.log('REGRESA HEROES DE MARVEL: ', heroes)
+  test('getHeroById debe retornar un heroe con id existente en la data(heroes)', () => {
+    const id = 1
+    const heroe = getHeroById(id)
+    const heroeData = heroes.find(h => h.id === id)
+
+    console.log('heroe obtenido: ', heroe)
+    console.log('heroe data existente: ', heroeData)
+    expect(heroe).toBe(heroeData)
   })
 })
