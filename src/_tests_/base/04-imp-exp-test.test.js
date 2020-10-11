b4c764816de94a5a262d23893be12c7a2998ba1a
César Aparicio <clark012389@hotmail.com>
2020-10-11 01:58:13 -0500

    Pruebas para comprobar la existencia de heroes de marvel y heroes que no sean de dc o marvel

--- src/_tests_/base/04-imp-exp-test.test.js
+++ src/_tests_/base/04-imp-exp-test.test.js
@@ -31,8 +31,28 @@ describe('Pruebas en 04-imp-exp', () => {
 
     expect(heroesDc).toEqual(heroesDcData)
   })
+
+  test('getHeroByOwner debe retornar un arreglo con heroes de Marvel', () => {
+    const owner = 'Marvel'
+    const heroesMarvel = getHeroByOwner(owner)
+
+    console.log('Heroes de Marvel ', heroesMarvel)
+    console.log('Longitud arreglo heroes de Marvel ', heroesMarvel.length)
+
+    expect(heroesMarvel.length).toBe(2) // [2]
+  })
+
+  test('getHeroByOwner debe retornar undefined si no existen heroes con el owner requerido', () => {
+    const owner = 'ANIME'
+    const heroesMarvel = getHeroByOwner(owner) // Puede no enviarse ningun argumento tambien
+
+    console.log('heroe obtenido: ', heroesMarvel)
+
+    expect(heroesMarvel.length).toBe(0)
+  })
 })
 
 /**
  * [1] - Hacemos uso de toBe() porque 'undefined' es un primitivo
+ * [2] - Se hace un toBe porque sabemos que de los 5 heroes, existen solo 2 de marvel, por ello experamos tener 2 como la longitud de los resultados obtenidos
  */
